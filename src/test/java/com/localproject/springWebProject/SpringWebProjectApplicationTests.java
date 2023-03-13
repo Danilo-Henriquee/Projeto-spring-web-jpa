cf363799340448fdf74fccc7c2fa7105b1a4d0dd
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-13 14:02:50 -0300

    Project created

---
+++ src/test/java/com/localproject/springWebProject/SpringWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localproject.springWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
