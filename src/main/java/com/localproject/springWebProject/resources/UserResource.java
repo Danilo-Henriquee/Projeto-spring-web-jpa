b41e1e1683874194e659ffc407ea3a58a703b48c
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-14 14:05:12 -0300

    User entity and resource

---
+++ src/main/java/com/localproject/springWebProject/resources/UserResource.java
@@ -0,0 +1,53 @@
+package com.localproject.springWebProject.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.localproject.springWebProject.entities.User;
+
+/*
+ * Para dizer que essa classe UserResource é um recurso web que é
+ * implementado por um controlador rest usa essa anotação
+ * @RestController
+ * 
+ * @RestController vai retornar o objeto e os dados desse objeto
+ * no formato JSON para consumo
+ */
+@RestController
+/*
+ * Além disso é preciso dar um nome para o recurso e nessa anotação vai ser
+ * inserido o caminho do recurso e como é um recurso relacionado a entidade user
+ * então vai ser chamado de "/users"
+ */
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	/*
+	 * Esse método vai ser o end-point para acessar os dados, primeiro vai retornar
+	 * um ResponseEntity que é um tipo especifico do Spring para retornar respostas
+	 * de requisições web, esse ResponseEntity ele é um generics ele espera um tipo
+	 * e o tipo dessa resposta vai ser a classe User, então vai ser retornado um
+	 * recurso do tipo User no formato JSON
+	 */
+	
+	/* 
+	 * Para indicar que esse método findAll() vai ser um método
+	 * que responde a requisição do tipo get do http utiliza-se 
+	 * o método @GetMapping
+	 * o GET no HTTP ele é responsavel por receber dados de um resource 
+	 * que no caso o resource é a entidade User
+	 */
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		/*
+		 * Aqui o responseEntity esta utilizando o método ok() 
+		 * para retornar a response com sucesso no http e utiliza o 
+		 * .body() para retornar o corpo da resposta e passando
+		 * como parametro o u que é o usuario criado 
+		 */
+		return ResponseEntity.ok().body(u);
+	}
+}
