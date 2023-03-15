28853e3b58b28fa5f6c6341c2c2cd5c20fa1f9f2
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-15 14:59:46 -0300

    Category added

---
+++ src/main/java/com/localproject/springWebProject/repositories/CategoryRepository.java
@@ -0,0 +1,23 @@
+package com.localproject.springWebProject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.localproject.springWebProject.entities.Category;
+
+/* 
+ * Essa interface ja possui todas as operações SQL especificas
+ * para a entidade User.
+ * Para utiliza-la ela herda da classe JpaRepository passando nos
+ * seus generics qual vai ser o tipo de que as operações SQL irão
+ * fazer, que no caso é o User, e no segundo generic, é o tipo da
+ * chave primaria que deve ser igual ao da entidade User
+ * 
+ * UserRepository também esta tendo injeção autowired
+ * porém como é uma implementação do JpaRepository ela ja herda esse
+ * componente da mesma tornando a anotação @Repository opcional
+ */
+
+//@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
