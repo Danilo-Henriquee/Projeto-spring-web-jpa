b9e996ccba68d01082458156e1bce2d76f0451d7
Danilo Henrique <danilomhenrique311@gmail.com>
2023-03-16 14:04:37 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/localproject/springWebProject/config/TestConfig.java
+++ src/main/java/com/localproject/springWebProject/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.localproject.springWebProject.entities.Category;
 import com.localproject.springWebProject.entities.Order;
+import com.localproject.springWebProject.entities.OrderItem;
 import com.localproject.springWebProject.entities.Product;
 import com.localproject.springWebProject.entities.User;
 import com.localproject.springWebProject.entities.enums.OrderStatus;
 import com.localproject.springWebProject.repositories.CategoryRepository;
+import com.localproject.springWebProject.repositories.OrderItemRepository;
 import com.localproject.springWebProject.repositories.OrderRepository;
 import com.localproject.springWebProject.repositories.ProductRepository;
 import com.localproject.springWebProject.repositories.UserRepository;
@@ -52,6 +54,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -81,8 +86,13 @@ public class TestConfig implements CommandLineRunner{
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	
